058534da4e5687cbe63b9126ecf312f59aab0b8d
Jason Allum <jallum@tonicdesign.com>
2015-04-06 11:10:19 -0400

    Initial Commit

---
+++ Promises/Promises.h
@@ -0,0 +1,19 @@
+//
+//  Promises.h
+//  Promises
+//
+//  Created by Jason Allum on 4/6/15.
+//  Copyright (c) 2015 Tonic Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promises.
+FOUNDATION_EXPORT double PromisesVersionNumber;
+
+//! Project version string for Promises.
+FOUNDATION_EXPORT const unsigned char PromisesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promises/PublicHeader.h>
+
+

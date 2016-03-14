6494130698acb2d2372ad078e914033fba25a8b7
Russell Stephens <rstephens@tonicdesign.com>
2016-03-14 14:24:40 -0400

    moving test to iOS target

--- PromisesTests/PromisesTests.swift
+++ PromisesTests/PromisesTests.swift
@@ -1,6 +1,5 @@
 import XCTest
 import Promises
-import UIKit
 
 enum PromisesTestsError: ErrorType {
     case TestError
@@ -106,34 +105,6 @@ class PromisesTests: XCTestCase {
         let resolved: Promise<[Int]> = when(promises)
         
         resolved.error { error in
-            expectation.fulfill()
-        }
-        
-        waitForExpectationsWithTimeout(3) { error in
-        }
-    }
-    
-    func test5() {
-        
-        let expectation = expectationWithDescription("promise completes")
-        
-        var promises: [Promise<UIView>] = []
-        
-        for (var i = 0; i < 3; i++) {
-            let p = promise() { () -> UIView in
-                let rect = CGRect(
-                    origin: CGPoint(x: 0, y: 0),
-                    size: CGSize(width: 10, height: 10)
-                )
-                let view = UIView(frame: rect)
-                return view
-            }
-            promises.append(p)
-        }
-        
-        let resolved: Promise<[UIView]> = when(promises)
-        
-        resolved.then { views in
             expectation.fulfill()
         }
         
--- PromisesiOSTests/PromisesiOSTests.swift
+++ PromisesiOSTests/PromisesiOSTests.swift
@@ -1,26 +1,34 @@
 import XCTest
+import UIKit
+import Promises
 
 class PromisesiOSTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func test1() {
+        
+        let expectation = expectationWithDescription("promise completes")
+        
+        var promises: [Promise<UIView>] = []
+        
+        for (var i = 0; i < 3; i++) {
+            let p = promise() { () -> UIView in
+                let rect = CGRect(
+                    origin: CGPoint(x: 0, y: 0),
+                    size: CGSize(width: 10, height: 10)
+                )
+                let view = UIView(frame: rect)
+                return view
+            }
+            promises.append(p)
+        }
+        
+        let resolved: Promise<[UIView]> = when(promises)
+        
+        resolved.then { views in
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(3) { error in
         }
     }
     
